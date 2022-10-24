6c215ef4f2dc0b10af7361a576c8ea252a60489c
Cas Knook <nwkcoins@gmail.com>
2022-10-24 22:32:10 +0200

    Homework-5

--- lessons/lesson-05/VulcanoCoin.sol
+++ lessons/lesson-05/VulcanoCoin.sol
@@ -1,57 +1,44 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0; /*2*/
 
-contract VulcanoCoin { /*4*/
-    uint256 totalSupply = 10000; /*5*/
+// added
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    address owner; /*8*/
+contract VulcanoCoin is Ownable /*added*/ {
+    uint256 totalSupply = 10000;
 
-    mapping(address => uint256) public /*14 (1)*/ balances; /*13*/
+    mapping(address => uint256) public balances;
 
-    /*18*/
     struct Payment {
         address recipient;
         uint256 amount;
     }
 
-    mapping(address => Payment[]) public userPayments; /*19a*/
+    mapping(address => Payment[]) public userPayments;
 
-    /*9*/
-    modifier onlyOwner() {
-        if (msg.sender == owner) {
-            _;
-        }
+    event logSupply(uint256);
+    event logTransfer(address, uint256);
+
+    // changed to inherit constructor from parent Ownable.sol
+    constructor() Ownable() {
+        balances[owner()] = totalSupply;
     }
 
-    event logSupply(uint256); /*12a*/
-    event logTransfer(address, uint256); /*17*/
-
-    /*11*/
-    constructor() {
-        owner = msg.sender;
-        balances[owner] = totalSupply; /*15*/
-    }
-
-    /*6*/
     function getTotalSupply() public view returns(uint256) {
         return totalSupply;
     }
 
-    /*7*/
-    function increaseSupply() public onlyOwner /*10*/ {
+    function increaseSupply() public onlyOwner {
         totalSupply += 1000;
-        emit logSupply(totalSupply); /*12b*/
+        emit logSupply(totalSupply);
     }
 
-    /*14 (2)*/
     function getBalance(address _user) public view returns(uint256){
         require(_user != address(0), "Invalid address!");
         return balances[_user];
     }
 
-    /*16*/
     function transfer(address _recipient, uint256 _amount) public payable {
-        // if senders address (msg.sender) was a parameter, it could be any address
         require(_recipient != address(0), "Invalid address");
         require(_amount > 0, "Invalid amount!");
         require(balances[msg.sender] >= _amount, "User has not enough supply!");
@@ -64,7 +51,6 @@ contract VulcanoCoin { /*4*/
         emit logTransfer(_recipient, _amount);
     }
 
-    /*19 extra*/
     function getUserPayments(address _user) public view returns(Payment[] memory) {
         require(_user != address(0), "Invalid address!");
         return userPayments[_user];
